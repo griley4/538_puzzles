2ae8bcb55902037cf7223905f1613c74896f9ea4
Grant Riley <griley4@vols.utk.edu>
2016-06-29 16:53:57 -0400

    Final, seems to work

--- python/RobotPizzaPlot.py
+++ python/RobotPizzaPlot.py
@@ -8,5 +8,5 @@ for filename in sys.argv[1:]:
   with open(filename,'rt') as sf:
     data = pd.read_csv(sf, header=None, delim_whitespace=True)
 #    print (data[2])
-    pyplot.hist(data[2])
+    pyplot.hist(data[2], normed=1, bins=[3,4,5,6,7,8,9])
 pyplot.show()
