e43f2eb885471bd2234759bde3d438df6c472f15
Grant Riley <griley4@vols.utk.edu>
2016-11-24 14:47:32 -0500

    working. lol

--- bin/LonelyKing.cc
+++ bin/LonelyKing.cc
@@ -5,32 +5,45 @@
 #include <time.h>
 #include <vector>
 int main(){
-  int N = 65000;
-  int r1,sumvec;
-  std::vector<std::vector<int>> game(1,std::vector<int>(N));
+  int tests = 10000;
+  int sumres = 0;
   srand (time(NULL));
-  int s = game[0].size();
-  std::cout<<s<<std::endl;
-  while( s > 1){
-    for(int r=1; r<30; ++r){
-      game.push_back( std::vector<int>(s));
+  std::vector<int> result(tests);
+  for (int test=0;test<tests;++test){
+    bool go=1;
+    int N = 2;
+    int s = N;
+    int r=0;
+    int r1,sumvec=0;
+    std::vector<std::vector<int>> game(10,std::vector<int>(N));
+    while(go){
+      if (s > 1){
         for(int i=0; i<s; ++i){
-          r1 = rand() % s-1;
-          if (r==r1){
-            r1 += 1;
-            game[r][r1] = 1;
+          r1 = rand() % s;
+          if (i==r1){
+            r1 = (r1+1)%s;
           }
-          else{
-            game[r][r1] = 1;
-          }
+          game[r][r1] = 1;
         }
-      sumvec=0;
-      for( std::vector<int>::iterator it = game[r].begin(); it != game[r].end(); ++it){
-        sumvec += *it;
-        std::cout<<sumvec<<std::endl;
+        sumvec=0;
+        for( std::vector<int>::iterator it = game[r].begin(); it != game[r].end(); ++it){
+          sumvec += *it;
+          //  std::cout<<*it<<" tot: "<<sumvec<<std::endl;
+        }
+        //      std::cout<<"Round: "<<r<<" Players: "<<game[r].size()<<" Minus: "<<sumvec<<std::endl;
+        ++r;
+        s = s-sumvec;
+        game[r] =  std::vector<int>(s);
       }
-      std::cout<<"Round: "<<r<<" Players: "<<game[r].size()<<" Minus: "<<sumvec<<std::endl;
+      else{
+        go=0;
+        std::cout<<"Round: "<<r<<" Players: "<<game[r].size()<<std::endl;
+        result[test] = game[r].size();
+      }
     }
-    s= s-sumvec;
   }
+  for( std::vector<int>::iterator it2 = result.begin(); it2 != result.end(); ++it2){
+    sumres += *it2;
+  }
+  std::cout<<"Wins: "<<sumres<<" Losses: "<< tests - sumres<<std::endl;
 }
